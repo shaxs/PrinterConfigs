62714e642853753b2e637761c889d0ba7b6614f9
shaxs <shaxs@crazyshaxs.com>
2025-06-17 14:27:08 -0700

    Autocommit from 2025-06-17 14:27:08
    Klipper version: v0.13.0-152-gf54b7b93
    
    Moonraker version: v0.9.3-77-gccbfd64
    
    Mainsail version: v2.14.0

--- autocommit.sh
+++ autocommit.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-#######################################################################
-## NOTE: This script originates from here but I tweaked the pull     ##
-## command, changed default location for backup, and added a comment ##
-## for reference later.                                              ##
-#######################################################################
+# This script can now perform two actions:
+# 'push' (default): Commits and pushes the current config to GitHub.
+# 'fetch': Fetches and resets the local config to match the remote repo.
 
 #####################################################################
 ### Please set the paths accordingly. In case you don't have all  ###
 ### the listed folders, just keep that line commented out.        ###
 #####################################################################
-### Path to your config folder you want to backup
+### Path to your config folder
 config_folder=~/printer_data/config
-
-# NOTE: The above should work for just about everyone, but a somewhat
-# recent update to moonraker changed paths, etc. You can run the 
-# provided moonraker script 'data-path-fix.sh' to fix/update
-# older installs
 
 ### Path to your Klipper folder, by default that is '~/klipper'
 klipper_folder=~/klipper
@@ -27,19 +20,10 @@ moonraker_folder=~/moonraker
 ### Path to your Mainsail folder, by default that is '~/mainsail'
 mainsail_folder=~/mainsail
 
-### Path to your Fluidd folder, by default that is '~/fluidd'
-#fluidd_folder=~/fluidd
-
-### The branch of the repository that you want to save your config
-### By default that is 'main'
+### The branch of the repository that you want to use
 branch=main
 
 db_file=~/printer_data/database/moonraker-sql.db
-
-#####################################################################
-#####################################################################
-
-
 
 #####################################################################
 ################ !!! DO NOT EDIT BELOW THIS LINE !!! ################
@@ -57,39 +41,38 @@ grab_version(){
     mainsail_ver=$(head -n 1 $mainsail_folder/.version)
     m3="Mainsail version: $mainsail_ver"
   fi
-  if [ ! -z "$fluidd_folder" ]; then
-    fluidd_ver=$(head -n 1 $fluidd_folder/.version)
-    m4="Fluidd version: $fluidd_ver"
-  fi
 }
 
-# Here we copy the sqlite database for backup
-# To RESTORE the database, stop moonraker, then use the following command:
-# cp ~/printer_data/config/moonraker-sql.db ~/printer_data/database/
-# Finally, restart moonraker
-
-if [ -f $db_file ]; then
-   echo "sqlite based history database found! Copying..."
-   cp ~/printer_data/database/moonraker-sql.db ~/printer_data/config/
-else
-   echo "sqlite based history database not found"
-fi
-
-# To fully automate this and not have to deal with auth issues, generate a legacy token on Github
-# then update the command below to use the token. Run the command in your base directory and it will
-# handle auth. This should just be executed in your shell and not committed to any files or
-# Github will revoke the token. =)
-# git remote set-url origin https://XXXXXXXXXXX@github.com/EricZimmerman/Voron24Configs.git/
-# Note that that format is for changing things after the repository is in use, vs initially
-
 push_config(){
+  # Copy database backup
+  if [ -f $db_file ]; then
+     echo "sqlite based history database found! Copying..."
+     cp ~/printer_data/database/moonraker-sql.db ~/printer_data/config/
+  else
+     echo "sqlite based history database not found"
+  fi
+  
+  # Add, commit, and push changes
   cd $config_folder
   git pull origin $branch --no-rebase
   git add .
   current_date=$(date +"%Y-%m-%d %T")
-  git commit -m "Autocommit from $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
+  git commit -m "Autocommit from $current_date" -m "$m1" -m "$m2" -m "$m3"
   git push origin $branch
 }
 
-grab_version
-push_config
+fetch_config(){
+  cd $config_folder
+  git fetch
+  git reset --hard origin/$branch
+}
+
+# Check for argument passed to the script
+if [ "$1" = "fetch" ]; then
+  echo "Fetching latest config from GitHub..."
+  fetch_config
+else
+  echo "Backing up config to GitHub..."
+  grab_version
+  push_config
+fi
